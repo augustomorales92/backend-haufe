273467cce5983257867d4c13d32bd72e275d48aa
augustomorales92 <augustomorales92@gmail.com>
2024-02-28 13:28:07 +0100

    hide port

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import db from './index'
 import { unmatchedRoute } from './middlewares/errorhandler'
 
 const app = express()
-const port = 3001
+const port = process.env.PORT
 
 app.use(cors())
 app.use(helmet())
