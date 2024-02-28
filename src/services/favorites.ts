5f7eb86542d4410e62b47d431448d5c713235b51
augustomorales92 <augustomorales92@gmail.com>
2024-02-28 18:15:34 +0100

    rollback to fix issue

--- src/services/favorites.ts
+++ src/services/favorites.ts
@@ -11,32 +11,20 @@ export const getFavoritesByUser = async ({ id }): Promise<number[]> => {
   }
 }
 
-export const getFavorite = async ({ id, favoriteId }): Promise<Favorite> => {
-    try {
-      const favorite= await Favorite.findOne({ where: { user_id: id, favorite_id: favoriteId } })
-      return favorite
-    } catch (e) {
-      console.error(e)
-      throw new Error(e)
-    }
-  }
-
 export const addFavorite = async ({ favoriteId, id }): Promise<Error | void> => {
   try {
-    const foundedFavorite = await getFavorite({id, favoriteId})
-    if(!foundedFavorite) await Favorite.create({ user_id: id, favorite_id: favoriteId })
+    await Favorite.create({ user_id: id, favorite_id: favoriteId })
   } catch (e) {
     console.error(e)
-    throw Error(e)
+    throw new Error(e)
   }
 }
 
 export const deleteFavorite = async ({ favoriteId, id }): Promise<Error | void> => {
   try {
-    const foundedFavorite = await getFavorite({id, favoriteId})
-    if(foundedFavorite) await Favorite.destroy({ where: { user_id: id, favorite_id: favoriteId } })
+    await Favorite.destroy({ where: { user_id: id, favorite_id: favoriteId } })
   } catch (e) {
     console.error(e)
-    throw Error(e)
+    throw new Error(e)
   }
 }
