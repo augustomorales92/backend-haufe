f19c79db185fa5ef212c94eb0da97add9da120d8
augustomorales92 <augustomorales92@gmail.com>
2024-02-28 11:31:42 +0100

    fix mysql2 error

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv'
 import { Sequelize } from 'sequelize'
+import mysql2 from 'mysql2'
 
 dotenv.config()
 
 const sequelize = new Sequelize({
   dialect: 'mysql',
+  dialectModule: mysql2,
   host: process.env.DATABASE_HOST!,
   port: Number(process.env.DATABASE_PORT),
   username: process.env.DATABASE_USERNAME!,
